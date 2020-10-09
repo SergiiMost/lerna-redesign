879f5768322d5453bd56b715e19a8fd79431a8f1
SergiiMost <s.v.mostovoyi@gmail.com>
2020-10-09 16:56:11 -0400

    update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (async () => {
-  const responseObj = await fetch("http://api.github.com/repos/lerna/lerna")
+  const responseObj = await fetch("https://api.github.com/repos/lerna/lerna")
   const data = await responseObj.json()
   const num1 = data.stargazers_count.toString().slice(0, 2)
   const num2 = data.stargazers_count.toString().slice(2, 3)
